8d0339657e66f8f6070384ab89267ceb4b9e5261
Wiiitek <kubiczak.jakub@gmail.com>
2016-10-15 22:02:51 +0100

    AET-11 PROCESSING_ERROR - userSettingsDirective is not needed in expandable panel directive

--- report/src/main/webapp/app/layout/main/url/expandablePanel.directive.js
+++ report/src/main/webapp/app/layout/main/url/expandablePanel.directive.js
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-define(['angularAMD', 'userSettingsService'], function (angularAMD) {
+define(['angularAMD'], function (angularAMD) {
   'use strict';
   angularAMD.directive('aetExpandablePanel', expandablePanelDirective);
 
