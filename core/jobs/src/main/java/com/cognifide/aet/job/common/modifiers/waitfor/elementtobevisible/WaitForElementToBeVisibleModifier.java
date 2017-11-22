b9530044543b6023dcb0274c7b101f7ffc8dfcf1
Volodymyr Vashchuk <volodymyr.vashchuk@cognifide.com>
2017-11-22 15:00:56 +0100

    Reduced code in wait-for modifiers and extended them with WebElementsLocatorParams

--- core/jobs/src/main/java/com/cognifide/aet/job/common/modifiers/waitfor/elementtobevisible/WaitForElementToBeVisibleModifier.java
+++ core/jobs/src/main/java/com/cognifide/aet/job/common/modifiers/waitfor/elementtobevisible/WaitForElementToBeVisibleModifier.java
@@ -21,8 +21,8 @@ import com.cognifide.aet.communication.api.metadata.CollectorStepResult;
 import com.cognifide.aet.job.api.collector.CollectorJob;
 import com.cognifide.aet.job.api.exceptions.ParametersException;
 import com.cognifide.aet.job.api.exceptions.ProcessingException;
+import com.cognifide.aet.job.common.modifiers.WebElementsLocatorParams;
 import com.cognifide.aet.job.common.modifiers.waitfor.WaitForHelper;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.slf4j.Logger;
@@ -30,21 +30,13 @@ import org.slf4j.LoggerFactory;
 
 import java.util.Map;
 
-public class WaitForElementToBeVisibleModifier implements CollectorJob {
+public class WaitForElementToBeVisibleModifier extends WebElementsLocatorParams implements CollectorJob {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(WaitForElementToBeVisibleModifier.class);
 
     static final String NAME = "wait-for-element-to-be-visible";
 
     private final WebDriver webDriver;
-
-    private static final String CSS_PARAMETER = "css";
-    private static final String XPATH_PARAMETER = "xpath";
-    private static final String TIMEOUT_PARAMETER = "timeout";
-
-    private String webElementCssLocator;
-    private String webElementXpathLocator;
-    private int timeoutInMilliseconds;
 
     WaitForElementToBeVisibleModifier(WebDriver webDriver) {
         this.webDriver = webDriver;
@@ -54,8 +46,8 @@ public class WaitForElementToBeVisibleModifier implements CollectorJob {
     public CollectorStepResult collect() throws ProcessingException {
         CollectorStepResult result;
         try {
-            By locator = WaitForHelper.determineLocatorStrategy(webElementCssLocator, webElementXpathLocator);
-            result = WaitForHelper.waitForExpectedCondition(webDriver, timeoutInMilliseconds, ExpectedConditions.visibilityOfElementLocated(locator));
+            result = WaitForHelper.waitForExpectedCondition(webDriver, getTimeoutInSeconds(),
+                    ExpectedConditions.visibilityOfElementLocated(getLocator()));
         } catch (Exception e) {
             final String message =
                     String.format("Failed to wait for element to be visible with provided locator. Error: %s",
@@ -67,18 +59,6 @@ public class WaitForElementToBeVisibleModifier implements CollectorJob {
     }
 
     @Override
-    public void setParameters(Map<String, String> parameters) throws ParametersException {
-        if (parameters.containsKey(CSS_PARAMETER)) {
-            webElementCssLocator = parameters.get(CSS_PARAMETER);
-        } else if (parameters.containsKey(XPATH_PARAMETER)) {
-            webElementXpathLocator = parameters.get(XPATH_PARAMETER);
-        } else {
-            throw new ParametersException("Xpath or CSS locator has to be provided for wait-for-element-to-be-visible modifier.");
-        }
-
-        if (!parameters.containsKey(TIMEOUT_PARAMETER)) {
-            throw new ParametersException("Timeout has to be provided for wait-for-element-to-be-visible modifier.");
-        }
-        timeoutInMilliseconds = Integer.parseInt(parameters.get(TIMEOUT_PARAMETER));
+    public void setParameters(Map<String, String> params) throws ParametersException {
     }
 }
--- core/jobs/src/main/java/com/cognifide/aet/job/common/modifiers/waitfor/imagecompletion/WaitForImageCompletionModifier.java
+++ core/jobs/src/main/java/com/cognifide/aet/job/common/modifiers/waitfor/imagecompletion/WaitForImageCompletionModifier.java
@@ -1,14 +1,14 @@
 /**
  * AET
- *
+ * <p>
  * Copyright (C) 2013 Cognifide Limited
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * <p>
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -21,31 +21,23 @@ import com.cognifide.aet.communication.api.metadata.CollectorStepResult;
 import com.cognifide.aet.job.api.collector.CollectorJob;
 import com.cognifide.aet.job.api.exceptions.ParametersException;
 import com.cognifide.aet.job.api.exceptions.ProcessingException;
+import com.cognifide.aet.job.common.modifiers.WebElementsLocatorParams;
 import com.cognifide.aet.job.common.modifiers.waitfor.WaitForHelper;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.By;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Map;
 
-public class WaitForImageCompletionModifier implements CollectorJob {
+public class WaitForImageCompletionModifier extends WebElementsLocatorParams implements CollectorJob {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(WaitForImageCompletionModifier.class);
 
     static final String NAME = "wait-for-image-completion";
 
     private final WebDriver webDriver;
-
-    private static final String CSS_PARAMETER = "css";
-    private static final String XPATH_PARAMETER = "xpath";
-    private static final String TIMEOUT_PARAMETER = "timeout";
-
-    private String webElementCssLocator;
-    private String webElementXpathLocator;
-    private int timeoutInMilliseconds;
 
     WaitForImageCompletionModifier(WebDriver webDriver) {
         this.webDriver = webDriver;
@@ -55,13 +47,14 @@ public class WaitForImageCompletionModifier implements CollectorJob {
     public CollectorStepResult collect() throws ProcessingException {
         CollectorStepResult result;
         try {
-            final By locator = WaitForHelper.determineLocatorStrategy(webElementCssLocator, webElementXpathLocator);
-            result = WaitForHelper.waitForExpectedCondition(webDriver, timeoutInMilliseconds, new ExpectedCondition<Boolean>() {
-                public Boolean apply(WebDriver webDriver) {
-                    return (Boolean) ((JavascriptExecutor) webDriver).executeScript("return arguments[0].complete",
-                            webDriver.findElement(locator));
-                }
-            });
+            result = WaitForHelper.waitForExpectedCondition(webDriver, getTimeoutInSeconds(),
+                    new ExpectedCondition<Boolean>() {
+                        public Boolean apply(WebDriver webDriver) {
+                            return (Boolean) ((JavascriptExecutor) webDriver).executeScript(
+                                    "return arguments[0].complete",
+                                    webDriver.findElement(getLocator()));
+                        }
+                    });
         } catch (Exception e) {
             final String message =
                     String.format("Failed to wait for image to be loaded with provided locator. Error: %s",
@@ -74,17 +67,5 @@ public class WaitForImageCompletionModifier implements CollectorJob {
 
     @Override
     public void setParameters(Map<String, String> parameters) throws ParametersException {
-        if (parameters.containsKey(CSS_PARAMETER)) {
-            webElementCssLocator = parameters.get(CSS_PARAMETER);
-        } else if (parameters.containsKey(XPATH_PARAMETER)) {
-            webElementXpathLocator = parameters.get(XPATH_PARAMETER);
-        } else {
-            throw new ParametersException("Xpath or CSS locator has to be provided for wait-for-image modifier.");
-        }
-
-        if (!parameters.containsKey(TIMEOUT_PARAMETER)) {
-            throw new ParametersException("Timeout has to be provided for wait-for-image modifier.");
-        }
-        timeoutInMilliseconds = Integer.parseInt(parameters.get(TIMEOUT_PARAMETER));
     }
 }
