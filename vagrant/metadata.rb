f0828bfac82afe01757c0d9569888a433c3adc49
Wiiitek <kubiczak.jakub@gmail.com>
2018-01-08 22:44:44 +0100

    update for cookbook version

--- vagrant/metadata.rb
+++ vagrant/metadata.rb
@@ -24,4 +24,4 @@ description      'Installs/Configures aet-vagrant'
 long_description 'Installs/Configures aet-vagrant'
 version          '1.4.9'
 
-depends 'aet', '~> 1.4.16-SNAPSHOT'
+depends 'aet', '~> 2.0.0-SNAPSHOT'
