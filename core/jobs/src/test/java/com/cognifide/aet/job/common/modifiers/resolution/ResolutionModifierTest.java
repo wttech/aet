2cafbda4a9f6834f5c5665c9c03c43bb2f6db467
Piotr Orleanski <porleanski@outlook.com>
2018-07-31 12:19:56 +0200

    ResolutionModifierTest - update

--- core/jobs/src/test/java/com/cognifide/aet/job/common/modifiers/resolution/ResolutionModifierTest.java
+++ core/jobs/src/test/java/com/cognifide/aet/job/common/modifiers/resolution/ResolutionModifierTest.java
@@ -15,10 +15,14 @@
  */
 package com.cognifide.aet.job.common.modifiers.resolution;
 
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
 import com.cognifide.aet.job.api.exceptions.ParametersException;
 import com.cognifide.aet.job.api.exceptions.ProcessingException;
 import java.util.Map;
-
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -29,8 +33,6 @@ import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.RemoteWebDriver;
-
-import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
 public class ResolutionModifierTest {
@@ -155,7 +157,7 @@ public class ResolutionModifierTest {
 
     verify(windowDimension, never()).getWidth();
     verify(windowDimension, never()).getHeight();
-    verify(window, atLeastOnce()).setSize(new Dimension(CUSTOM_WIDTH, CUSTOM_HEIGHT));
+    verify(window, times(1)).setSize(new Dimension(CUSTOM_WIDTH, CUSTOM_HEIGHT));
   }
 
   @Test
@@ -169,6 +171,6 @@ public class ResolutionModifierTest {
 
     verify(windowDimension, never()).getWidth();
     verify(windowDimension, never()).getHeight();
-    verify(window, atLeastOnce()).setSize(new Dimension(CUSTOM_WIDTH, BROWSER_HEIGHT_LIMIT));
+    verify(window, times(1)).setSize(new Dimension(CUSTOM_WIDTH, BROWSER_HEIGHT_LIMIT));
   }
 }
