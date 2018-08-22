d45e939458241f07ecba4256a2e91a86e1d45b79
Marcin Suchorski <marcin.suchorski@student.put.poznan.pl>
2018-08-22 09:45:48 +0200

    History popup is now a modal

--- report/src/main/webapp/app/layout/toolbar/toolbarTop.controller.js
+++ report/src/main/webapp/app/layout/toolbar/toolbarTop.controller.js
@@ -17,35 +17,33 @@
  */
 define([], function () {
   'use strict';
-  return ['$rootScope', '$http', 'suiteInfoService', 'metadataAccessService',
+  return ['$rootScope', '$uibModal', 'suiteInfoService', 'metadataAccessService', 'historyService',
     ToolbarTopController
   ];
 
-  function ToolbarTopController($rootScope, $http, suiteInfoService,
-    metadataAccessService) {
+  function ToolbarTopController($rootScope, $uibModal, suiteInfoService,
+    metadataAccessService, historyService) {
     var vm = this;
 
     $rootScope.$on('metadata:changed', updateToolbar);
-    $("[data-toggle='popover']").popover({
+    $('[data-toggle="popover"]').popover({
       placement: 'bottom'
     });
 
-    var suiteHeaders;
-    getSuiteHistory(suiteHeaders, $rootScope, $http);
-    updateToolbar();
+    historyService.fetchHistory(suiteInfoService.getInfo().version, function() {
+      var currentVersion = suiteInfoService.getInfo().version;
+      getPreviousVersion(currentVersion);
+      getNextVersion(currentVersion);
+      updateToolbar();
+    });
 
     /***************************************
      ***********  Private methods  *********
      ***************************************/
 
     function updateToolbar() {
-      document.getElementsByClassName('suite-history-container')[0].style.display = 'none';
       vm.showSuiteHistory = function () {
-        if (isSuiteHistoryVisible()) {
-          document.getElementsByClassName('suite-history-container')[0].style.display = 'none';
-        } else {
-          document.getElementsByClassName('suite-history-container')[0].style.display = 'block';
-        }
+        displayHistoryModal();
       };
       vm.suiteInfo = suiteInfoService.getInfo();
       vm.suiteStatistics = metadataAccessService.getSuite();
@@ -59,105 +57,29 @@ define([], function () {
       }
     }
 
-    function buildApiPath($allParametersList) {
-      return 'http://aet-vagrant' + '/api' + '/history' + '?' +
-        $allParametersList[0] + '&' +
-        $allParametersList[1] + '&' +
-        $allParametersList[2];
+    function getPreviousVersion(currentVersion) {
+      vm.previousVersion = historyService.getPreviousVersion(currentVersion);
     }
 
-    function getPreviousVersion(suiteHeaders) {
-      for (var i = 0; i < suiteHeaders.length; i++) {
-        if(suiteHeaders[i].version === vm.suiteInfo.version) {
-          if(i + 1 < suiteHeaders.length) {
-            vm.previousVersion = suiteHeaders[i + 1].version;
-            break;
-          }
-        }
-      }
+    function getNextVersion(currentVersion) {
+      vm.nextVersion = historyService.getNextVersion(currentVersion);
     }
 
-    function getNextVersion(suiteHeaders) {
-      for (var i = 0; i < suiteHeaders.length; i++) {
-        if(suiteHeaders[i].version === vm.suiteInfo.version) {
-          if(i - 1 > 0) {
-            vm.nextVersion = suiteHeaders[i - 1].version;
-            break;
-          }
+    function displayHistoryModal() {
+      $uibModal.open({
+        animation: true,
+        templateUrl: 'app/layout/modal/history/historyModal.view.html',
+        controller: 'historyModalController',
+        controllerAs: 'historyModal',
+        resolve: {
+          model: function () {
+            return vm.model;
+          },
+          viewMode: function () {
+            return vm.viewMode;
+          },
         }
-      }
-    }
-
-    function getSuiteHistory(suiteHeaders, $rootScope, $http) {
-      $rootScope.data = 'test';
-      var cUrl = new URL(window.location.href);
-      var company = cUrl.searchParams.get('company');
-      var project = cUrl.searchParams.get('project');
-      var suite = cUrl.searchParams.get('suite');
-      if (suite === null) {
-        var correlationId = cUrl.searchParams.get('correlationId');
-        suite = correlationId.split('-')[2];
-      }
-      var allParametersList = ['company=' + company, 'project=' + project, 'suite=' + suite];
-      return $http({
-        method: 'GET',
-        url: buildApiPath(allParametersList),
-        headers: {
-          'Content-Type': 'text/plain'
-        }
-      }).then(function (response) {
-        $rootScope.data = response.data;
-        suiteHeaders = response.data;
-        for (var i = 0; i < suiteHeaders.length; i++) {
-          var version = suiteHeaders[i].version;
-          var company = suiteHeaders[i].correlationId.split("-")[0];
-          var project = suiteHeaders[i].correlationId.split("-")[1];
-          var suite = suiteHeaders[i].correlationId.split("-")[2];
-          var correlationId = suiteHeaders[i].correlationId.split("-")[3];
-          suiteHeaders[i] = {
-            company: company,
-            project: project,
-            suite: suite,
-            version: version,
-            correlationId: correlationId,
-            selectedVersion: null,
-            isRebased: false,
-          }
-          if (typeof suiteHeaders[i - 1] !== "undefined") {
-            if (suiteHeaders[i].correlationId === suiteHeaders[i - 1].correlationId) {
-              suiteHeaders[i - 1].isRebased = true;
-            }
-          }
-          suiteHeaders[i].selectedVersion = document.querySelector('.suite-name-container>.preformatted').innerHTML;
-        }
-        $rootScope.suiteHeaders = suiteHeaders;
-        var reportPath = window.location.href;
-        var reportUrl = new URL(reportPath);
-        var currentVersion = reportUrl.searchParams.get('version');
-        getPreviousVersion(suiteHeaders);
-        getNextVersion(suiteHeaders);
-        if (currentVersion === null) {
-          $rootScope.reportPath = location.protocol + "//" + location.host + location.pathname + window.location.search;
-        } else {
-          reportPath = window.location.search.split('&');
-          reportPath = reportPath[0] + "&" + reportPath[1] + "&" + reportPath[2];
-          $rootScope.reportPath = location.protocol + "//" + location.host + location.pathname + reportPath;
-        }
-      }, function errorCallback(response) {
-        $rootScope.fullSuiteName = response.data['message'];
       });
-    }
-
-    function isSuiteHistoryVisible() {
-      if (typeof document.getElementsByClassName('suite-history-container')[0] !== 'undefined') {
-        if (document.getElementsByClassName('suite-history-container')[0].style.display === 'block') {
-          return true;
-        } else {
-          return false;
-        }
-      } else {
-        return false;
-      }
     }
   }
 });
