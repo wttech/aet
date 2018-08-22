48c1563b1756008ecb620d1e3dcd1c9fc541382e
Piotr Orleanski <porleanski@outlook.com>
2018-08-22 12:23:57 +0200

    refactor

--- test-executor/src/main/java/com/cognifide/aet/executor/SuiteRerun.java
+++ test-executor/src/main/java/com/cognifide/aet/executor/SuiteRerun.java
@@ -3,15 +3,13 @@ package com.cognifide.aet.executor;
 import static com.cognifide.aet.rest.BasicDataServlet.isValidCorrelationId;
 import static com.cognifide.aet.rest.BasicDataServlet.isValidName;
 
-import com.cognifide.aet.communication.api.metadata.Comparator;
-import com.cognifide.aet.communication.api.metadata.Step;
 import com.cognifide.aet.communication.api.metadata.Suite;
 import com.cognifide.aet.communication.api.metadata.Test;
-import com.cognifide.aet.communication.api.metadata.Url;
 import com.cognifide.aet.vs.DBKey;
 import com.cognifide.aet.vs.MetadataDAO;
 import com.cognifide.aet.vs.StorageException;
 import java.util.ArrayList;
+import java.util.Objects;
 import java.util.Optional;
 import javax.annotation.Nullable;
 import jdk.nashorn.internal.ir.annotations.Reference;
@@ -25,9 +23,11 @@ public class SuiteRerun {
   @Reference
   private static MetadataDAO metadataDAO;
 
-  private SuiteRerun(){}
+  private SuiteRerun() {
+  }
 
-  public static Suite getAndPrepareSuite(DBKey dbKey, String correlationId, String suiteName, @Nullable String testName) {
+  public static Suite getAndPrepareSuite(DBKey dbKey, String correlationId, String suiteName,
+      @Nullable String testName) {
     Suite suite = null;
     try {
       suite = getSuiteFromMetadata(dbKey, correlationId, suiteName);
@@ -56,25 +56,25 @@ public class SuiteRerun {
         .ifPresent(testToRerun -> {
           suite.removeAllTests();
           suite.addTest(testToRerun);
-          cleanDataFromSuite(suite);
+          cleanDataFromSuite(testToRerun);
         });
   }
 
-  private static void cleanDataFromSuite(Suite suite) {
-    for (Test test : suite.getTests()) {
-      for (Url url : test.getUrls()) {
-        url.setCollectionStats(null);
-        for (Step step : url.getSteps()) {
-          step.setStepResult(null);
-          if (step.getComparators() == null) {
-            continue;
-          }
-          for (Comparator comparator : step.getComparators()) {
-            comparator.setStepResult(null);
-            comparator.setFilters(new ArrayList<>());
-          }
-        }
-      }
-    }
+  private static void cleanDataFromSuite(Test test) {
+    test.getUrls().stream()
+        .forEach(url -> {
+          url.setCollectionStats(null);
+          url.getSteps()
+              .forEach(step -> {
+                step.setStepResult(null);
+                step.getComparators().stream()
+                    .filter(Objects::nonNull)
+                    .forEach(comparator -> {
+                      comparator.setStepResult(null);
+                      comparator.setFilters(new ArrayList<>());
+                    });
+              });
+        });
   }
 }
+
