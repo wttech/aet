bdc4bd0eaa46affece89f07a6c66c9f29550ade1
plutasnyy <kamil.plucinski97@gmail.com>
2018-08-30 10:18:33 +0200

    prepared abstarctuion for strategy

--- core/runner/src/main/java/com/cognifide/aet/runner/processing/SuiteExecutionTask.java
+++ core/runner/src/main/java/com/cognifide/aet/runner/processing/SuiteExecutionTask.java
@@ -61,6 +61,6 @@ public class SuiteExecutionTask extends ProcessorStrategy {
   }
 
   protected Suite getObjectToRun(){
-    return (Suite) objectToRun;
+    return (Suite) objectToRun.getObjectToRun();
   }
 }
