54e7c3ffc999124c89f07d1fbf659c30dbe7ba43
plutasnyy <kamil.plucinski97@gmail.com>
2018-09-07 11:52:02 +0200

    added test for suite suite run index wrapper

--- core/runner/src/test/java/com/cognifide/aet/runner/processing/data/RunIndexWrappers/SuiteRunIndexWrapperTest.java
+++ core/runner/src/test/java/com/cognifide/aet/runner/processing/data/RunIndexWrappers/SuiteRunIndexWrapperTest.java
@@ -15,7 +15,19 @@
  */
 package com.cognifide.aet.runner.processing.data.RunIndexWrappers;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.when;
+
+import com.cognifide.aet.communication.api.metadata.Suite;
+import com.cognifide.aet.communication.api.metadata.Url;
+import com.cognifide.aet.communication.api.wrappers.MetadataRunDecorator;
 import com.cognifide.aet.communication.api.wrappers.Run;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -28,19 +40,82 @@ public class SuiteRunIndexWrapperTest {
   private SuiteRunIndexWrapper suiteRunIndexWrapper;
 
   @Mock
-  Run objectToRun;
+  Run objectToRunWrapper;
+
+  @Mock
+  private Suite suite;
+
+  @Mock
+  private com.cognifide.aet.communication.api.metadata.Test test;
+
+  @Mock
+  private com.cognifide.aet.communication.api.metadata.Test test2;
+
+  @Mock
+  private Url url;
+
+  @Mock
+  private Url url2;
 
   @Before
   public void setUp() throws Exception {
-    suiteRunIndexWrapper = new SuiteRunIndexWrapper(objectToRun);
+    suiteRunIndexWrapper = new SuiteRunIndexWrapper(objectToRunWrapper);
+    when(objectToRunWrapper.getRealSuite()).thenReturn(suite);
+    when(objectToRunWrapper.getObjectToRun()).thenReturn(suite);
   }
 
   @Test
-  public void getUrls() {
+  public void getUrls_expectZero() {
+    prepareZeroUrls();
+
+    ArrayList<MetadataRunDecorator> urlsResult = suiteRunIndexWrapper
+        .getUrls();
+    assertThat(urlsResult.size(), is(0));
   }
 
   @Test
-  public void countUrls() {
+  public void getUrls_expectThree() {
+    prepareThreeUrls();
+
+    ArrayList<MetadataRunDecorator> urlsResult = suiteRunIndexWrapper
+        .getUrls();
+    assertThat(urlsResult.size(), is(3));
   }
 
+  @Test
+  public void countUrls_expectZero(){
+    prepareZeroUrls();
+
+    assertThat(suiteRunIndexWrapper.countUrls(), is(0));
+  }
+
+  @Test
+  public void countUrls_expectThree() {
+    prepareThreeUrls();
+
+    assertThat(suiteRunIndexWrapper.countUrls(), is(3));
+  }
+
+  private void prepareZeroUrls(){
+    List<com.cognifide.aet.communication.api.metadata.Test> tests = new ArrayList<>();
+    tests.add(test);
+    tests.add(test2);
+    when(suite.getTests()).thenReturn(tests);
+  }
+
+  private void prepareThreeUrls(){
+    Set<Url> firstUrlsSet = new HashSet<>();
+    firstUrlsSet.add(url);
+    when(test.getUrls()).thenReturn(firstUrlsSet);
+
+    Set<Url> secondUrlsSet = new HashSet<>();
+    secondUrlsSet.add(url);
+    secondUrlsSet.add(url2);
+    when(test2.getUrls()).thenReturn(secondUrlsSet);
+
+    List<com.cognifide.aet.communication.api.metadata.Test> tests = new ArrayList<>();
+    tests.add(test);
+    tests.add(test2);
+    when(suite.getTests()).thenReturn(tests);
+  }
 }
