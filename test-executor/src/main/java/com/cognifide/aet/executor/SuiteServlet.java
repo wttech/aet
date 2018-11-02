8863efe51b2a9d668408f64b75bfaa3c3b8a2455
Waldemar Gregorczyk <waldemar.gregorczyk@cognifide.com>
2018-11-02 10:32:59 +0100

    Gson field optimization

--- test-executor/src/main/java/com/cognifide/aet/executor/SuiteServlet.java
+++ test-executor/src/main/java/com/cognifide/aet/executor/SuiteServlet.java
@@ -3,15 +3,12 @@
  *
  * Copyright (C) 2013 Cognifide Limited
  *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
- * in compliance with the License. You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at
  *
  * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software distributed under the License
- * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
- * or implied. See the License for the specific language governing permissions and limitations under
- * the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
  */
 package com.cognifide.aet.executor;
 
@@ -67,6 +64,8 @@ public class SuiteServlet extends HttpServlet {
 
   private static final String PROJECT_CHECK_SUM = "projectChecksSum";
 
+  private static final Gson gson = new Gson();
+
 
   @Reference
   private HttpService httpService;
@@ -82,7 +81,6 @@ public class SuiteServlet extends HttpServlet {
   protected void doPost(HttpServletRequest request, HttpServletResponse response)
       throws IOException {
     if (ServletFileUpload.isMultipartContent(request)) {
-      Gson gson = new Gson();
       Map<String, String> requestData = getRequestData(request);
       final String suite = requestData.get(SUITE_PARAM);
       final String name = requestData.get(NAME_PARAM);
