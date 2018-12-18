1a0ce0d517484826104563a4dc304b2dc203eaab
Maciej Laskowski <skejven@gmail.com>
2018-12-18 14:15:01 +0100

    unify label and service name

--- core/worker/src/main/java/com/cognifide/aet/worker/listeners/WorkersListenersServiceConfig.java
+++ core/worker/src/main/java/com/cognifide/aet/worker/listeners/WorkersListenersServiceConfig.java
@@ -19,7 +19,7 @@ import org.osgi.service.metatype.annotations.AttributeDefinition;
 import org.osgi.service.metatype.annotations.AttributeType;
 import org.osgi.service.metatype.annotations.ObjectClassDefinition;
 
-@ObjectClassDefinition(name = "AET Workers Listener Factory")
+@ObjectClassDefinition(name = "AET Workers Listener Service")
 public @interface WorkersListenersServiceConfig {
 
   String COLLECTORS_NO_ENV = "COLLECTORS_NO";
