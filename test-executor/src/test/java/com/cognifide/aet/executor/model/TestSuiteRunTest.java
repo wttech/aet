686670801c144a75471290464520cf3eb2b92acc
Wojciech Blachowski <wojciech.blachowski@cognifide.com>
2019-01-18 13:02:50 +0100

    Add correlationId regex assertion

--- test-executor/src/test/java/com/cognifide/aet/executor/model/TestSuiteRunTest.java
+++ test-executor/src/test/java/com/cognifide/aet/executor/model/TestSuiteRunTest.java
@@ -5,22 +5,26 @@ import org.junit.Test;
 import java.util.Collections;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.equalTo;
 
 public class TestSuiteRunTest {
 
   @Test
   public void constructor_whenOverridingNameAndDomain_expectUpdatedValues() {
-    TestSuiteRun baseSuiteRun = new TestSuiteRun("abcd", "copmany", "project", "ijkl", Collections.emptyList());
+    String company = "company";
+    String project = "project";
+    String name = "abcd";
+    String domain = "ijkl";
+    TestSuiteRun baseSuiteRun = new TestSuiteRun(name, company, project, domain, Collections.emptyList());
 
     String newName = "efgh";
     String newDomain = "mnop";
     TestSuiteRun updatedSuiteRun = new TestSuiteRun(baseSuiteRun, newName, newDomain, Collections.emptyList());
+    String correlationRegex = String.format("^%s-%s-%s-[0-9]*$", company, project, newName);
 
     assertThat(updatedSuiteRun.getName(), equalTo(newName));
     assertThat(updatedSuiteRun.getDomain(), equalTo(newDomain));
-    assertThat(updatedSuiteRun.getCorrelationId(), containsString(newName));
+    assertThat(updatedSuiteRun.getCorrelationId(), updatedSuiteRun.getCorrelationId().matches(correlationRegex));
   }
 
 }
