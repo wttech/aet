618b94d6b09d24acbc4f89edc3c261cd40f28760
Wiktor Jurczyszyn <wiktor.jurczyszyn@cognifide.com>
2020-07-02 13:05:31 +0200

    #566 updated CHANGELOG

--- report/src/main/webapp/app/services/endpointConfiguration.service.js
+++ report/src/main/webapp/app/services/endpointConfiguration.service.js
@@ -21,7 +21,8 @@ define(['angularAMD'], function (angularAMD) {
     return {
       getEndpoint: function () {
         var config = {
-          'getUrl': '/api/'
+          'getUrl': 'http://localhost:8181/api/'
+          // 'getUrl': '/api/'
           //'getUrl': 'http://aet-vagrant/api/'
         };
         return config;
