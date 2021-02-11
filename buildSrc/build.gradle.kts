493f4bfc6509d2c0d88c20210fec85d50ea8bb2a
Bartosz Zbytniewski <bartosz.zbytniewski.dev@gmail.com>
2021-02-11 14:26:32 +0100

    fix: Move java-conventions to kts

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    // Support convention plugins written in Groovy. Convention plugins are build scripts in 'src/main' that automatically become available as plugins in the main build.
-    id("groovy-gradle-plugin")
+    // Support convention plugins written in Kotlin. Convention plugins are build scripts in 'src/main' that automatically become available as plugins in the main build.
+    `kotlin-dsl`
 }
 
 repositories {
