dbc83217ddb7e78c3ce0b3b9bfa7809b720de1ab
radoslaw.lawgmin <radoslaw.lawgmin@cognifide.com>
2021-04-23 13:57:27 +0200

    company name change

--- report/src/main/webapp/app/layout/main/url/caseFactory.service.js
+++ report/src/main/webapp/app/layout/main/url/caseFactory.service.js
@@ -1,7 +1,7 @@
 /*
  * AET
  *
- * Copyright (C) 2013 Cognifide Limited
+ * Copyright (C) 2021 Wunderman Thompson Technology
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
